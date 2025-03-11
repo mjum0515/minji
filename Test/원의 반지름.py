22e4c86cac8fa6f8d82a9945d4f715a7ba537905
minji <mjum0515@naver.com>
2025-03-11 14:25:50 +0900

    first commit

---
+++ Test/원의 반지름.py
@@ -0,0 +1,8 @@
+Python 3.12.2 (v3.12.2:6abddd9f6a, Feb  6 2024, 17:02:06) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('원의 반지름', 5.0)
+원의 반지름 5.0
+>>> print('원의 면적', 3.14 * 5.0 * 5.0)
+원의 면적 78.5
+>>> print('원의 둘레', 2.0 * 3.14 * 5.0)
+원의 둘레 31.400000000000002
